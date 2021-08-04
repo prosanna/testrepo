3be8be1bd27f317b07d7595c3c35fa1371c98cb7
prosanna <prosanna2005@gmail.com>
2021-08-05 03:56:31 +0530

    [J8IA]: Lambda Syntax commit 2

--- MyWork/src/javaEightInAction/chapter3/LambdaSyntax.java
+++ MyWork/src/javaEightInAction/chapter3/LambdaSyntax.java
@@ -18,13 +18,27 @@ public class LambdaSyntax {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		test s = () -> "Prosanna";
+		test1 t1 = ()->{};
+		test2 t2 = () -> "Prosanna";//Expression
+		test2 s = () -> {return "Prosanna";};//explicit return statement.
+		test3 t3 = (Integer a) -> {return "Prosanna" + a;};// return is a Control-flow statement.
 		System.out.println(s.check());
+		System.out.println(t3.check(Integer.valueOf(69)));
 
 	}
 
 	@FunctionalInterface
-	interface test {
+	interface test1 {
+		void check();
+	}
+
+	@FunctionalInterface
+	interface test2 {
 		String check();
+	}
+
+	@FunctionalInterface
+	interface test3 {
+		String check(Integer i);
 	}
 }
