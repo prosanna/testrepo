ddf9dcc8f1ab489fc08ac9949ca7df06c2d6f3ee
prosanna <prosanna2005@gmail.com>
2021-08-28 02:03:30 +0530

    java list problem solution commited.

--- MyWork/src/hackerrank/problems/javaProb/easy/JavaList.java
+++ MyWork/src/hackerrank/problems/javaProb/easy/JavaList.java
@@ -4,7 +4,8 @@
 package hackerrank.problems.javaProb.easy;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Scanner;
@@ -23,15 +24,46 @@ public class JavaList {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+
+		Scanner st = new Scanner(System.in);
+		LinkedList<Integer> LL = new LinkedList<Integer>();
+		int n = st.nextInt();
+		for (int i = 0; i < n; i++) {
+			int p = st.nextInt();
+			LL.add(p);
+		}
+		int q = st.nextInt();
+		while (q-- > 0) {
+			st.nextLine();
+			String s = st.nextLine();
+			if (s.equals("Insert")) {
+				int index = st.nextInt();
+				int num = st.nextInt();
+				LL.add(index, num);
+			} else {
+				int num = st.nextInt();
+				LL.remove(num);
+			}
+		}
+
+		LL.forEach(p -> System.out.print(p.toString() + " "));
+		System.out.println("\n");
+	}
+
+	/**
+	 * TODO
+	 */
+	public static void alternativeSol() {
+
 		Scanner scan = new Scanner(System.in);
 		int N = scan.nextInt();
-		List<Integer> numList = new ArrayList<Integer>();
+		List<Integer> numList = new LinkedList<Integer>();
 		// Second line input
 		for (int i = 0; i < N; i++) {
 			numList.add(scan.nextInt());
 		}
 		// Third line input
-		Map<String, List<Integer>> qryMap = new HashMap<>();
+		Map<String, List<Integer>> qryMap = new LinkedHashMap<>();
 		int numQry = scan.nextInt();
 		for (int i = 0; i < numQry; i++) {
 			String query = scan.next();
@@ -53,12 +85,12 @@ public class JavaList {
 
 				numList.add(tmpL.get(0), tmpL.get(1));
 			} else {
-				numList.remove(tmpL.get(0));
+				numList.remove(tmpL.get(0).intValue());
 			}
 		}
 
-		/* numList.forEach(p -> System.out.println(p.toString())); */
-		numList.forEach(System.out::println);
+		numList.forEach(p -> System.out.print(p.toString() + " "));
+		/* numList.forEach(System.out::print); */
 
 	}
 
