3f6db42133587669e0d5164c78a6a443c3855856
prosanna <prosanna2005@gmail.com>
2021-09-03 02:02:33 +0530

    Code in progress

---
+++ MyWork/src/hackerrank/problems/javaProb/collection/JavaPriorityQueue.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package hackerrank.problems.javaProb.collection;
+
+/**
+ * <b>Java Priority Queue</b>
+ * <p>
+ * In computer science, a priority queue is an abstract data type which is like
+ * a regular queue, but where additionally each element has a "priority"
+ * associated with it. In a priority queue, an element with high priority is
+ * served before an element with low priority.
+ * 
+ * @author prosanna
+ * @see <a href=
+ *      "https://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html">https://docs.oracle.com/javase/7/docs/api/java/util/PriorityQueue.html</a>
+ */
+public class JavaPriorityQueue {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
