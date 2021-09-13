0ba90f86695286636a845cab6ea4d64526e56e95
Prosanna Mandal <prosannam@nrifintech.com>
2021-09-13 19:38:22 +0530

    code commited

--- MyWork/src/java5Features/staticImport/MethodOverLoadingExample.java
+++ MyWork/src/java5Features/staticImport/MethodOverLoadingExample.java
@@ -15,25 +15,25 @@ public class MethodOverLoadingExample {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		test("prosanna",90);
+		test("prosanna", 90.07);
 		test("arko");
 
 	}
-	
+
 	public static void test(String a, Integer b) {
-		System.out.println("Print Integer: "+a+" "+b);
+		System.out.println("Print Integer: " + a + " " + b);
 	}
-	
+
 	public static void test(String a, BigDecimal b) {
-		System.out.println("Print BigDecimal: "+a+" "+b);
+		System.out.println("Print BigDecimal: " + a + " " + b);
 	}
-	
+
 	public static void test(String a, Number b) {
-		System.out.println("Print Number: "+a+" "+b);
+		System.out.println("Print Number: " + a + " " + b);
 	}
-	
+
 	public static void test(String a) {
-		System.out.println("Print String: "+a);
+		System.out.println("Print String: " + a);
 	}
 
 }
